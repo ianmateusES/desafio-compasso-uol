e3fd30eecdcf9ecb9a9015e68f0b589be9327867
ianmateusES <ec.ianmateus@alu.ufc.br>
2021-06-07 00:15:02 -0300

    refactor: type correction

--- api/src/shared/infra/typeorm/utils/objectAttributeFilter.ts
+++ api/src/shared/infra/typeorm/utils/objectAttributeFilter.ts
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { omitBy, isNil } from 'lodash';
 import { Raw } from 'typeorm';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
 const objectAttributeFilter = (obj: Object): Object => {
   const where = {};
-  Object.keys(omitBy(obj, isNil)).forEach(item => {
-    where[item] = Raw(
-      dateField => `lower(${dateField}) = lower('${obj[item]}')`,
+  Object.keys(omitBy(obj, isNil)).forEach((item: any) => {
+    [where as any][item] = Raw(
+      dateField => `lower(${dateField}) = lower('${[obj as any][item]}')`,
     );
   });
   return { where };
